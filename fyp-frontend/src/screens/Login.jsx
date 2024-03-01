17fd42cfd53f26108cc4e2de13bc6f11746b8816
hammadaslam1 <hammadaslam308@gmail.com>
2024-03-01 22:25:01 +0500

    added Login

--- fyp-frontend/src/screens/Login.jsx
+++ fyp-frontend/src/screens/Login.jsx
@@ -1,17 +1,27 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Card, Input, Typography } from "@mui/material";
+import { LoginCSS } from "../styles/LoginCSS";
 
 const Login = () => {
-    return ( 
-        <Box className='loginBox'>
+    return (
+        <Box sx={LoginCSS.loginBox}>
             <Box>
-                <Typography className="loginLeftHeading">Welcome to My System</Typography>
+                <Typography className="loginLeftHeading">
+                    Welcome to My System
+                </Typography>
                 <Typography>A short descriptions</Typography>
             </Box>
             <Box>
-                
+                <Card sx={LoginCSS.loginCard} elevation={20}>
+                    <Box sx={LoginCSS.loginFields}>
+                        <Typography variant="h3">Login</Typography>
+                        <Input type="email" />
+                        <Input type="password" />
+                        <Button variant="contained">Login</Button>
+                    </Box>
+                </Card>
             </Box>
         </Box>
-     );
-}
- 
+    );
+};
+
 export default Login;
---
+++ fyp-frontend/src/styles/LoginCSS.jsx
@@ -0,0 +1,17 @@
+export const LoginCSS = {
+    loginBox: {
+        display: "flex",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        minHeight: "100vh",
+    },
+    loginCard: {
+        padding: 5,
+        borderRadius: 8,
+    },
+    loginFields:{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
+};
