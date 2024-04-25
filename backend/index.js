a5f1eb96dac23ff3ddaaea2e59d89667279970e2
Muhammad Hammad Aslam <hammadaslam308@gmail.com>
2024-04-25 22:02:51 +0500

    added routes to the backend

---
+++ backend/index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import { MONGODB } from '../ENV.js'
+import cookieParser from 'cookie-parser';
+import path from 'path';
+
+
+dotenv.config();
+
+
+mongoose
+    .connect(MONGODB, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => console.log('connected')).catch((e) => console.log(e)).finally(() => console.log('start'));
+
+const __dirname = path.resolve();
+
+const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(3001, () => {
+    console.log('Server is running on port 3001!');
+});
+
+// app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
