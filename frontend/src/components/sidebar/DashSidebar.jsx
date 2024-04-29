4a4f6749f0a39e3b1fbea2ef558afac8db9f6472
Muhammad Hammad Aslam <hammadaslam308@gmail.com>
2024-04-29 21:35:47 +0500

    updated sidebar

--- frontend/src/components/sidebar/DashSidebar.jsx
+++ frontend/src/components/sidebar/DashSidebar.jsx
@@ -5,7 +5,6 @@ import { toggleTab } from "../../redux/tabs/TabSlice";
 import { Box, Tab, Tabs } from "@mui/material";
 import { DashboardCSS } from "../../styles/DashboardCSS";
 
-
 const DashSidebar = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -16,38 +15,35 @@ const DashSidebar = () => {
     const tabFromUrl = urlParams.get("tab");
     dispatch(toggleTab(tabFromUrl));
   }, [dispatch, location.search]);
+  const allTabs = [
+    { label: "Profile", value: 0, name: "profile" },
+    { label: "Account", value: 1, name: "account" },
+    { label: "Dashboard", value: 2, name: "dashboard" },
+    { label: "Sign out", value: 3, name: "signout" },
+  ];
   return (
-    <Box sx={{flex: 1}}>
+    <Box sx={{ flex: 1 }}>
       <Tabs
         orientation="vertical"
         variant="scrollable"
         aria-label="Vertical tabs example"
         sx={[DashboardCSS.sideMain, { borderRight: 1, borderColor: "divider" }]}
       >
-        <Tab
-          sx={
-            tabs === "profile"
-              ? [DashboardCSS.tabTitle, DashboardCSS.tabActive]
-              : DashboardCSS.tabTitle
-          }
-        //   icon={<AccountBoxIcon sx={DashboardCSS.tabIcon} />}
-          iconPosition="start"
-          label="Profile"
-          value={0}
-        //   onClick={() => navigate(PROFILE)}
-        />
-        <Tab
-          sx={
-            tabs === "signout"
-              ? [DashboardCSS.tabTitle, DashboardCSS.tabActive]
-              : DashboardCSS.tabTitle
-          }
-        //   icon={<LogoutIcon sx={DashboardCSS.tabIcon} />}
-          iconPosition="start"
-          label="Sign out"
-          value={1}
-        //   onClick={() => navigate(SIGNOUT)}
-        />
+        {allTabs.map((data, i) => (
+          <Tab
+            key={i}
+            sx={
+              tabs === data.name
+                ? [DashboardCSS.tabTitle, DashboardCSS.tabActive]
+                : DashboardCSS.tabTitle
+            }
+            //   icon={<AccountBoxIcon sx={DashboardCSS.tabIcon} />}
+            iconPosition="start"
+            label={data.label}
+            value={data.value}
+            //   onClick={() => navigate(PROFILE)}
+          />
+        ))}
       </Tabs>
     </Box>
   );
