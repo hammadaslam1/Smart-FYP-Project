f4e648aa27aeac69b4ddd3f196e488111a085fe9
Muhammad Hammad Aslam <hammadaslam308@gmail.com>
2024-05-25 17:57:20 +0500

    minor changes

--- frontend/src/components/progress/PieGraph.jsx
+++ frontend/src/components/progress/PieGraph.jsx
@@ -7,9 +7,11 @@ const PieGraph = () => {
           {
             data: [
               { id: 0, value: 10, label: "series A" },
-              { id: 1, value: 15, label: "series B" },
-              { id: 2, value: 20, label: "series C" },
-              { id: 3, value: 30, label: "series D" },
+              { id: 1, value: 20, label: "series B" },
+              { id: 2, value: 30, label: "series C" },
+              { id: 3, value: 40, label: "series D" },
+              { id: 4, value: 50, label: "series D" },
+              { id: 5, value: 15, label: "series D" },
             ],
             innerRadius: 10,
             outerRadius: 150,
--- frontend/src/screens/student/StudentHome.jsx
+++ frontend/src/screens/student/StudentHome.jsx
@@ -1,10 +1,17 @@
 import { Card, Typography } from "@mui/material";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { DASHBOARD } from "../../routes/Routes";
 const StudentHome = () => {
-    return (
-        <Card>
-            <Typography variant="h2">student</Typography>
-        </Card>
-    );
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate(DASHBOARD);
+  }, []);
+  return (
+    <Card>
+      <Typography variant="h2">student</Typography>
+    </Card>
+  );
 };
 
 export default StudentHome;
