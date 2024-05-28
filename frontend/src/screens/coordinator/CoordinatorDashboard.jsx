5cbded0664f1e05cd40cf3eea2307b5feffe1db8
Ahsan Usman <ahsanusmanofficial@gmail.com>
2024-05-29 00:00:57 +0500

    Connected dashboard with router

--- frontend/src/screens/coordinator/CoordinatorDashboard.jsx
+++ frontend/src/screens/coordinator/CoordinatorDashboard.jsx
@@ -1,14 +1,40 @@
 import { Box } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const CoordinatorDashboard = () => {
+  const navigate = useNavigate();
   return (
     <Box sx={{ pt: 10 }}>
       <h1>Coordinator Dashboard</h1>
       <div className="buttonsDiv">
-        <button className="dashboardBigButton">Project Idea Approval</button>
-        <button className="dashboardBigButton">Evaluate Weekly Progress</button>
-        <button className="dashboardBigButton">Evaluate Documentation</button>
-        <button className="dashboardBigButton">
+        <button
+          className="dashboardBigButton"
+          
+        >
+          Project Idea Approval
+        </button>
+        <button
+          className="dashboardBigButton"
+          onClick={() => {
+            navigate("/weekly-progress-evaluation");
+          }}
+        >
+          Evaluate Weekly Progress
+        </button>
+        <button
+          className="dashboardBigButton"
+          onClick={() => {
+            navigate("/report-evaluation");
+          }}
+        >
+         Report Evaluation
+          
+        </button>
+        {/* //yahan jugaar hua hai Evaluate Documentation asli name */}
+        <button
+          className="dashboardBigButton"
+          
+        >
           View Student Final Report
         </button>
       </div>
--- frontend/src/screens/student/StudentDashboard.jsx
+++ frontend/src/screens/student/StudentDashboard.jsx
@@ -12,7 +12,9 @@ import PieGraph from "../../components/progress/PieGraph";
 import Gauge from "../../components/progress/Guage";
 import "../../styles/studentcomponentsstyles/StudentDashboard.css";
 import PrimaryButton from "../../components/buttons/PrimaryButton";
+import { useNavigate } from "react-router-dom";
 const StudentDashboard = () => {
+  const navigate = useNavigate();
   return (
     <Box sx={{ pt: 10 }}>
       <Typography variant="h4" sx={{ color: "#08422D", p: 3, fontWeight: 600 }}>
@@ -27,16 +29,16 @@ const StudentDashboard = () => {
         }}
         elevation={0}
       >
-        <PrimaryButton sx={{ width: "33%", height: "150px", my: 1 }}>
+        <PrimaryButton onClick={()=>{navigate("/group-formation")}} sx={{ width: "33%", height: "150px", my: 1 }}>
           Create Project Group
         </PrimaryButton>
-        <PrimaryButton sx={{ width: "33%", height: "150px", my: 1 }}>
+        <PrimaryButton onClick={()=>{navigate("/idea-submission")}} sx={{ width: "33%", height: "150px", my: 1 }}>
           Submit Project Idea
         </PrimaryButton>
-        <PrimaryButton sx={{ width: "33%", height: "150px", my: 1 }}>
+        <PrimaryButton onClick={()=>{navigate("/documentation-submission")}} sx={{ width: "33%", height: "150px", my: 1 }}>
           Submit Documentation
         </PrimaryButton>
-        <PrimaryButton sx={{ width: "33%", height: "150px", my: 1 }}>
+        <PrimaryButton onClick={()=>{navigate("/add-weekly-progress")}} sx={{ width: "33%", height: "150px", my: 1 }}>
           Submit Weekly Progress Report
         </PrimaryButton>
         <PrimaryButton sx={{ width: "33%", height: "150px", my: 1 }}>
