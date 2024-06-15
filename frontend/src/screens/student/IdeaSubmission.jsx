6978766d37f8217da2c5f4de5cc6c0e086a49748
Ahsan Usman <ahsanusmanofficial@gmail.com>
2024-06-15 09:34:49 +0500

    working on idea submission module

--- frontend/src/screens/student/IdeaSubmission.jsx
+++ frontend/src/screens/student/IdeaSubmission.jsx
@@ -2,8 +2,13 @@ import { Box, Card, TextField, Typography } from "@mui/material";
 import "../../styles/studentcomponentsstyles/ideasubmission.css";
 import SignupInput from "../../components/inputs/SignupInput";
 import PrimaryButton from "../../components/buttons/PrimaryButton";
+import {useSelector} from 'react-redux'
 import { useState } from "react";
 const IdeaSubmission = () => {
+  const handleIdeaSubmission = () => {
+    alert(`Idea Title: ${title} \nIdea Description: ${description}`)
+  };
+  const { currentUser } = useSelector((state) => state.user);
   const [title,setTitle] = useState("")
   const [description,setDescription] = useState("")
   return (
@@ -11,11 +16,11 @@ const IdeaSubmission = () => {
       <Typography variant="h4" sx={{ p: 3, color: "#08422D", fontWeight: 600 }}>Project Idea Submission</Typography>
       <Card elevation={0} sx={{p:3}} >
         <Typography variant="body1">Project's Title:</Typography>
-      <SignupInput   color="success" />
+      <SignupInput   color="success" onChange={(e)=>{setTitle(e.target.value)}} />
       <Typography variant="body1">Project's Description:</Typography>
-      <TextField  color="success"  multiline minRows={3} sx={{ mx: 0 }} fullWidth></TextField>
+      <TextField  color="success"  onChange={(e)=>{setDescription(e.target.value)}}  multiline minRows={3} sx={{ mx: 0 }} fullWidth></TextField>
        <PrimaryButton
-       
+       onClick={handleIdeaSubmission}
        sx={{
         mt:2,
         height:"50px",
