63422ac94b89dddebf4a852ef3cc7f47225d4cb9
Ahsan Usman <ahsanusmanofficial@gmail.com>
2024-06-30 23:24:00 +0500

    redesigned group details page

--- frontend/src/screens/coordinator/GroupDetails.jsx
+++ frontend/src/screens/coordinator/GroupDetails.jsx
@@ -2,156 +2,168 @@ import { Box, Card, Typography } from "@mui/material";
 import { useLocation } from "react-router-dom";
 import PrimaryButton from "../../components/buttons/PrimaryButton";
 const GroupDetails = () => {
-
   const { state } = useLocation();
   console.log(state);
-  const handleFileFetch =  (id,filename) => {
-    fetch(`http://localhost:3001/api/groups/getdocument/${id}/${filename}`,{
+  const handleFileFetch = (id, filename) => {
+    fetch(`http://localhost:3001/api/groups/getdocument/${id}/${filename}`, {
       method: "GET",
-     
-    }).then(async (response)=>{
+    }).then(async (response) => {
       const blob = await response.blob();
-    const url = window.URL.createObjectURL(blob);
-    window.open(url, '_blank');
-    })
-   
-  }
+      const url = window.URL.createObjectURL(blob);
+      window.open(url, "_blank");
+    });
+  };
   return (
-    <Card sx={{ mt: 10, p: 3 }}>
-      <Box sx={{ width: "100%", border: "1px solid red" }}>
-        <Typography variant="h4" textAlign="center">
+    <Card
+      sx={{
+        m: 3,
+        mt: 13,
+        p: 3,
+        width: "100%",
+        border: "3px solid #08422D",
+        borderRadius: "20px",
+      }}
+      elevation={5}
+    >
+      <Typography variant="h4">Group Details</Typography>
+      <Box sx={{ width: "100%" }}>
+        <Typography
+          sx={{
+            fontWeight: 700,
+            fontSize: "3rem",
+            borderBottom: "1px solid grey",
+            paddingBottom: "10px",
+          }}
+          variant="h4"
+          textAlign="center"
+        >
           {state.idea.title}{" "}
         </Typography>
-        <Typography variant="body1">{state.idea.description} </Typography>
+        <Typography
+          variant="body1"
+          sx={{
+            padding: "20px",
+            wordWrap: "break-word",
+            borderBottom: "1px solid grey",
+            paddingBottom: "10px",
+          }}
+          textAlign="center"
+        >
+          {state.idea.description}{" "}
+          asjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskajasjdaskdjsajdaksjladskaj
+        </Typography>
       </Box>
       <Box
         sx={{
-          width: "100%",
-          border: "1px solid red",
           display: "flex",
+          borderBottom: "1px solid grey",
+          paddingBottom: "10px",
+          paddingTop: "10px",
           justifyContent: "space-around",
         }}
       >
-        <Typography variant="h4" textAlign="center">
-          Supervisor: {state.supervisor}{" "}
-        </Typography>
-        <Typography variant="h4" textAlign="center">
-          Team Lead: {state.teamLead}{" "}
-        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-around",
+          }}
+        >
+          <Typography variant="h6">
+            <span style={{ fontWeight: "700" }}>Class: </span>
+            {state.class}
+          </Typography>
+          <Typography variant="h6">
+            <span style={{ fontWeight: "700" }}>Team Lead: </span>
+            {state.teamLead}{" "}
+          </Typography>
+          <Typography variant="h6">
+            <span style={{ fontWeight: "700" }}>Supervisor: </span>
+            {state.supervisor}{" "}
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            display: "inline-block",
+            marginLeft: 1,
+          }}
+        >
+          <Typography
+            variant="h6"
+            sx={{ display: "inline-block", fontWeight: 700 }}
+          >
+            Team Members
+          </Typography>
+          {state.members.map((member, index) => (
+            <Typography variant="h6" textAlign="left">
+              {index + 1}.{" " + member.student_name}
+            </Typography>
+          ))}
+        </Box>
       </Box>
-      <Box 
+
+      <Box
         sx={{
           width: "100%",
-          border: "1px solid red",
         }}
       >
-        <Typography variant="h4">Class: {state.class}</Typography>
-        <Box sx={{ display: "flex" }}>
-          <Typography variant="h4" sx={{ display: "inline-block" }}>
-            Team Members:
-          </Typography>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              display: "inline-block",
-              marginLeft: 1,
-            }}
-          >
-            {state.members.map((member) => (
-              <Typography variant="h4" textAlign="left">
-                {member.student_name}
-              </Typography>
-            ))}
-          </Box>
-        </Box>
         <Box>
-          <Typography variant="h4" textAlign="center">
-            Documentation
-          </Typography>
-          <Box>
-            <Typography variant="h4" textAlign="center">
+          <Typography variant="h4" textAlign="center">Documentation</Typography>
+          <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+            <PrimaryButton
+              sx={{flex:1,margin:"5px" }}
+              onClick={() => {
+                handleFileFetch(
+                  state._id,
+                  state.documentation.proposal.filename
+                );
+              }}
+              disabled={state.documentation.proposal.filename?false:true}
+            >
               Proposal
-            </Typography>
-            <Box
-              sx={{
-                margin: "0px auto",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                width: "80%",
+            </PrimaryButton>
+
+            <PrimaryButton
+              sx={{ flex:1,margin:"5px" }}
+              onClick={() => {
+                handleFileFetch(
+                  state._id,
+                  state.documentation.defense.filename
+                );
               }}
+              disabled={state.documentation.proposal.filename?false:true}
             >
-              <Typography variant="h4">
-                {state.documentation.proposal.filename}
-              </Typography>
-              <PrimaryButton sx={{ width: "200px" }} onClick={()=>{
-                handleFileFetch(state._id,state.documentation.proposal.filename)
-              }}>Download</PrimaryButton>
-            </Box>
-          </Box>
-          <Box>
-            <Typography variant="h4" textAlign="center">
               Defense
-            </Typography>
-            <Box
-              sx={{
-                margin: "0px auto",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                width: "80%",
+            </PrimaryButton>
+
+            <PrimaryButton
+              sx={{ flex:1,margin:"5px"}}
+              onClick={() => {
+                handleFileFetch(
+                  state._id,
+                  state.documentation.final_documentation.filename
+                );
               }}
+              disabled={state.documentation.proposal.filename?false:true}
             >
-              <Typography variant="h4">
-                {state.documentation.defense.filename}
-              </Typography>
-              <PrimaryButton sx={{ width: "200px" }} onClick={()=>{
-                handleFileFetch(state._id,state.documentation.proposal.filename)
-              }}>Download</PrimaryButton>
-            </Box>
-          </Box>
-          <Box>
-            <Typography variant="h4" textAlign="center">
-              Presentation
-            </Typography>
-            <Box
-              sx={{
-                margin: "0px auto",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                width: "80%",
-              }}
-            >
-              <Typography variant="h4">
-                {state.documentation.presentation.filename}
-              </Typography>
-              <PrimaryButton sx={{ width: "200px" }} onClick={()=>{
-                handleFileFetch(state._id,state.documentation.proposal.filename)
-              }}>Download</PrimaryButton>
-            </Box>
-          </Box>
-          <Box>
-            <Typography variant="h4" textAlign="center">
               Final Documentation
-            </Typography>
-            <Box
-              sx={{
-                margin: "0px auto",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                width: "80%",
+            </PrimaryButton>
+
+            <PrimaryButton
+              sx={{flex:1,margin:"5px" }}
+              onClick={() => {
+                handleFileFetch(
+                  state._id,
+                  state.documentation.presentation.filename
+                );
               }}
+              disabled={state.documentation.proposal.filename?false:true}
             >
-              <Typography variant="h4">
-                {state.documentation.final_documentation.filename}
-              </Typography>
-              <PrimaryButton sx={{ width: "200px" }} onClick={()=>{
-                handleFileFetch(state._id,state.documentation.proposal.filename)
-              }}>Download</PrimaryButton>
-            </Box>
+              Final Presentation
+            </PrimaryButton>
           </Box>
         </Box>
       </Box>
