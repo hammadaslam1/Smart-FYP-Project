0078fcc44332282ce672140b118b56499adf0cfe
Ahsan Usman <ahsanusmanofficial@gmail.com>
2024-07-20 11:23:04 +0500

    improved submit frs

--- frontend/src/components/frcomponents/SubmitFRs.jsx
+++ frontend/src/components/frcomponents/SubmitFRs.jsx
@@ -1,64 +1,80 @@
 import SignupInput from "../../components/inputs/SignupInput";
-import { Box, Button } from "@mui/material";
+import { Box, Button, TextField } from "@mui/material";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import PrimaryButton from "../buttons/PrimaryButton";
 
 const SubmitFRs = () => {
   const [FRCount, setFRCount] = useState([1]);
   const [FRs, setFRs] = useState([]);
   const student = useSelector((state) => state.student.student);
-    const group_id = student.group.group_id
-    console.log(group_id);
+  const group_id = student.group.group_id;
+  console.log(group_id);
   const handleFRs = (e, index) => {
     const updatedFRs = [...FRs];
-    updatedFRs[index] = { ...updatedFRs[index], [e.target.name]: e.target.value };
+    updatedFRs[index] = {
+      ...updatedFRs[index],
+      [e.target.name]: e.target.value,
+    };
     setFRs(updatedFRs);
-    console.log(FRs)
+    console.log(FRs);
   };
   const handleFRSave = () => {
-    fetch("http://localhost:3001/api/groups/submitfrs",{
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            FRs:FRs,
-            group_id: group_id
-        }),
-  
-    }).then((response)=>{
-        if(response.ok){
-            alert("Form submitted successfully")
-        }else{
-            alert("Failed to submit form")
-        }
-        console.log(response)
-      }).catch((error)=>{
+    fetch("http://localhost:3001/api/groups/submitfrs", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        FRs: FRs,
+        group_id: group_id,
+      }),
     })
-  }
+      .then((response) => {
+        if (response.ok) {
+          alert("Form submitted successfully");
+        } else {
+          alert("Failed to submit form");
+        }
+        console.log(response);
+      })
+      .catch((error) => {});
+  };
   return (
     <Box>
       <Box sx={{ width: "700px" }}>
+        <span style={{color:"red"}}>[Select Manage FRs if you have already entered the Functional Requirements]</span>
         {FRCount.map((data, i) => (
           <Box key={i}>
-            <SignupInput name={`FR${i + 1}`} onChange={(e) => handleFRs(e, i)} />
+            <TextField
+              sx={{ width: "100%", my: 1 }}
+              color="success"
+              multiline
+              minRows={1}
+              label={`FR${i + 1}`}
+              name={`FR${i + 1}`}
+              onChange={(e) => handleFRs(e, i)}
+              InputLabelProps={{
+                style: { fontWeight: "bold" },
+              }}
+            ></TextField>
           </Box>
         ))}
-        <Button
-          variant="outlined"
+        <PrimaryButton
+          sx={{ width: "60px", height: "50px" }}
           onClick={() => {
             setFRCount([...FRCount, FRCount.length + 1]);
             setFRs([...FRs, {}]);
           }}
         >
-          +
-        </Button>
-        <Button
-          variant="outlined"
+          <span style={{ fontSize: "2rem", padding: 0 }}>+</span>
+        </PrimaryButton>
+        <PrimaryButton
+          sx={{ width: "100px", height: "50px", marginLeft: "10px" }}
           onClick={handleFRSave}
         >
           Submit
-        </Button>
+        </PrimaryButton>
       </Box>
     </Box>
   );
